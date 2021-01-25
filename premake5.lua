da6bcfd05b138eeeb8dab9fd9560246c76cef569
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2021-01-25 11:12:35 +0000

    Added premake for Mango Engine

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.cpp",
+		"src/**.h",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "c++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "c++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
